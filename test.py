e3948e59cb9f2d3b584f7eb8ad3e13f05a802f6a
FilipKowalski <55386371+FilipKowalski@users.noreply.github.com>
2021-11-22 11:46:53 +0000

    added install packages file and printing out some info on a user (not working)

--- test.py
+++ test.py
@@ -1,1 +1,11 @@
-print("Hello, is python working")
+#inport Githum from the PyGithub library
+from github import Github
+#we initialise a PyGithub Github object with our access token
+githubObject = Github("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")
+
+#getting a user object and printing some trivial details
+user = githubObject.get_user()
+print("user:    " + user.login)
+print("fullname:" + user.name)
+print("location:" + user.location)
+print("company: " + user.login)
