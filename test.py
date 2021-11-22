325cacbe4aad0200b2c8554d4e5c7fb9afa6bc83
FilipKowalski <55386371+FilipKowalski@users.noreply.github.com>
2021-11-22 14:03:55 +0000

    added another package to getNeededPackages and added a docker-compose

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
-#inport Githum from the PyGithub library
-from github import Github
+from github import Github   #github api access
 import json                 #for dictionary to string
+import pymongo              #for mongodb access
 
 #we initialise a PyGithub Github object with our access token
 githubObject = Github("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")
@@ -15,3 +15,26 @@ userDict = {'user': user.login,
             }
 
 print ("dictionary is " + json.dumps(userDict))
+
+#store the dictionary in a mongodb
+
+#first we need to remove all null fields from the dictionary, because
+#if we dont we will end up with null fiends in the db. this will cause us
+#lots of debugging problems later. only store actual data in the database
+
+for k, v in dict(userDict).items():
+    if v is None:
+        del userDict[k]
+
+print ("cleaned dictionary is " + json.dumps(userDict))
+
+#now lets store the data
+
+#establish connection
+conn  ="mongodb://localhost:27017"
+client = pymongo.MongoClient(conn)
+
+#create a database
+db = client.classDB
+
+db.githubuser.insert_many([userDict])
