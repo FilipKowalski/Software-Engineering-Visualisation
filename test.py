ec5c3eca764d69af0e49eb3aab2ee6e356eb8f8e
FilipKowalski <55386371+FilipKowalski@users.noreply.github.com>
2021-11-22 12:06:33 +0000

    putting user data into a dictionary

--- test.py
+++ test.py
@@ -1,17 +1,17 @@
 #inport Githum from the PyGithub library
 from github import Github
+import json                 #for dictionary to string
+
 #we initialise a PyGithub Github object with our access token
-githubObject = Github("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")
+githubObject = Github("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")
 
-#getting a user object and printing some trivial details
+#getting a user object and building a data dictionary
 user = githubObject.get_user()
-print("user:     " + user.login)
 
-if user.name is not None :
-    print("fullname: " + user.name)
+userDict = {'user': user.login,
+            'fullname': user.name,
+            'location': user.location,
+            'company': user.company
+            }
 
-if user.location is not None :
-    print("location: " + user.location)
-
-if user.company is not None :
-    print("company:  " + user.company)
+print ("dictionary is " + json.dumps(userDict))
