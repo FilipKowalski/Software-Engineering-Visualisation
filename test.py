59c5654d902e21edffb2e7eb3dce2eaec7292e50
FilipKowalski <55386371+FilipKowalski@users.noreply.github.com>
2021-11-22 11:54:17 +0000

    printing out some user info, Working!

--- test.py
+++ test.py
@@ -1,11 +1,17 @@
 #inport Githum from the PyGithub library
 from github import Github
 #we initialise a PyGithub Github object with our access token
-githubObject = Github("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")
+githubObject = Github("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")
 
 #getting a user object and printing some trivial details
 user = githubObject.get_user()
-print("user:    " + user.login)
-print("fullname:" + user.name)
-print("location:" + user.location)
-print("company: " + user.login)
+print("user:     " + user.login)
+
+if user.name is not None :
+    print("fullname: " + user.name)
+
+if user.location is not None :
+    print("location: " + user.location)
+
+if user.company is not None :
+    print("company:  " + user.company)
