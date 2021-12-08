8fc4a049fd654fae79221edf60dc8123febf6837
FilipKowalski <55386371+FilipKowalski@users.noreply.github.com>
2021-12-08 13:51:28 +0000

    added a main and made getting commits and stroing hem in database a function

--- getData.py
+++ getData.py
@@ -1,38 +1,44 @@
-from github import Github   #github api access
-from faker import Faker     #for anonimysing names
-from collections import defaultdict
-from datetime import datetime
-import json                 #for dictionary to string
-import pymongo              #for mongodb access
-import os
+from github import Github           #github api access
+from faker import Faker             #for anonimysing names
+from collections import defaultdict #for storing data
+from datetime import datetime       #for changing datetime object to a string
+import json                         #for dictionary to string
+import pymongo                      #for mongodb access
+import os                           #for getting envoiroment variable
 
-#load the faker and its providers
-faker = Faker()
-names = defaultdict(faker.name)
+def getCommitsInRepoAndStoreToDb(repoName):
+    #getting a repository
+    repo = githubObject.get_repo(repoName)
 
-#we initialise a PyGithub Github object with our access token
-token = os.getenv("TOKEN")
-print(token)
-githubObject = Github(token)
+    #getting the commits from that repository
+    repoCommits = repo.get_commits()
 
-#getting a repo object
-repo = githubObject.get_repo("nadineel/CSU22012-Algorithm-Data-Structure-Group-Project")
+    for commit in repoCommits:
+        commitFiles = commit.files
+        fileChanges = 0
 
-repoCommits = repo.get_commits()
+        for file in commitFiles:
+            fileChanges += file.additions + file.deletions + file.changes
 
-for commit in repoCommits:
-    commitFiles = commit.files
-    fileChanges = 0
+        comDct = {
+            'commitedInRepoId': repo.id,
+            'commitAuthor': commit.commit.author.name,
+            'commitDateString':   (commit.commit.author.date).strftime("%Y/%m/%d"),
+            'totalChangesInCommit': fileChanges
+        }
+        for k, v in dict(comDct).items():
+            if v is None:
+                del comDct[k]
+        print("commit: " + json.dumps(comDct))
 
-    for file in commitFiles:
-        fileChanges += file.additions + file.deletions + file.changes
+if __name__ == "__main__":
+    #load the faker and its providers
+    faker = Faker()
+    names = defaultdict(faker.name)
 
-    comDct = {
-        'commitAuthor': commit.commit.author.name,
-        'commitDateString':   (commit.commit.author.date).strftime("%Y/%m/%d"),
-        'totalChangesInCommit': fileChanges
-    }
-    for k, v in dict(comDct).items():
-        if v is None:
-            del comDct[k]
-    print("commit: " + json.dumps(comDct))
+    #we initialise a PyGithub Github object with our access token
+    token = os.getenv("TOKEN")
+    print(token)
+    githubObject = Github(token)
+
+    getCommitsInRepoAndStoreToDb("nadineel/CSU22012-Algorithm-Data-Structure-Group-Project")
